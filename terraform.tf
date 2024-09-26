181ec5cb68a9d348be53dfdff80c9f6ef07f74ff
Dave Franco <davefranco1987@gmail.com>
2024-09-26 22:29:21 +0200

    fix kubectl provider

--- terraform.tf
+++ terraform.tf
@@ -53,6 +53,13 @@ provider "kubectl" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.this.token
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    command     = "aws"
+  }
+
 }
 
 
